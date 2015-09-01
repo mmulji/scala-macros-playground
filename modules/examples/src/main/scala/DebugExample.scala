8c2c01852b85d75edbb11192f7066692a79b4b73
Milan Mulji <milan.mulji@mmedia-cs.com>
2015-09-01 23:08:18 +0200

    Additional examples from Adam Warski blog

--- modules/examples/src/main/scala/DebugExample.scala
+++ modules/examples/src/main/scala/DebugExample.scala
@@ -5,4 +5,15 @@ object DebugExample extends App {
   import DebugMacros._
 
   hello()
+
+  val y = 10
+
+  def test() {
+    val p = 11
+    debug(p)
+    debug(p + y)
+  }
+
+  test()
+
 }
--- modules/macros/src/main/scala/DebugMacros.scala
+++ modules/macros/src/main/scala/DebugMacros.scala
@@ -10,4 +10,21 @@ object DebugMacros {
     reify { println("Hello World!") }
   }
 
+  def printparam(param: Any): Unit = macro printparam_impl
+
+  def printparam_impl(c: Context)(param: c.Expr[Any]): c.Expr[Unit] = {
+    import c.universe._
+    reify { println(param.splice) }
+  }
+
+  def debug(param: Any): Unit = macro debug_impl
+
+  def debug_impl(c: Context)(param: c.Expr[Any]): c.Expr[Unit] = {
+    import c.universe._
+    val paramRep = show(param.tree)
+    val paramRepTree = Literal(Constant(paramRep))
+    val paramRepExpr = c.Expr[String](paramRepTree)
+    reify { println(paramRepExpr.splice + " = " + param.splice) }
+  }
+
 }
