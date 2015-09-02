6470284159a6494fc9977d54242b7b93dcd53245
Milan Mulji <milan.mulji@mmedia-cs.com>
2015-09-02 10:50:05 +0200

    Separated prj, added test, still wip

--- modules/bizz/src/main/scala/NetworkDeps.scala
+++ modules/bizz/src/main/scala/NetworkDeps.scala
@@ -1,17 +1,20 @@
 
+object NetworkDeps {
+
+  class State(name: String)
+
+  case class One() extends State("One")
+  case class Two() extends State("Two")
+  case class Three() extends State("Three")
+  case class Four() extends State("Four")
 
 
-case class State(name: String)
+  class Network {
 
-case object One extends State("One")
-case object Two extends State("Two")
-case object Three extends State("Three")
-case object Four extends State("Four")
+    var states: Set[State] = null
+    var startingState: State = null
 
-
-class NetworkDeps {
-
-  var states: List[State] = null
-  var startingState: State = null
+  }
 
 }
+
